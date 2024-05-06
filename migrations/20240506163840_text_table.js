3158572d4ec5d461a6e82c35a2adf6dbcc83934e
Kirsten Lindsmith <kirstenlindsmith@gmail.com>
2024-05-06 12:57:12 -0400

    add partial send text endpoint

---
+++ migrations/20240506163840_text_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('messages', function (table) {
+    table.uuid('referral_uuid').notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('messages', function (table) {
+    table.dropColumn('referral_uuid');
+  });
+};
--- server.js
+++ server.js
@@ -2,12 +2,34 @@ const express = require('express');
 const app = express();
 const port = 1337;
 const { knex } = require('./knex');
+const referrals = require('./referrals.json');
 
-app.post('/send-text', async (req, res) => {});
+//parse JSON bodies
+app.use(express.json());
+
+//parse URL encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/send-text', async (req, res) => {
+  const { referral_uuid, text } = req.body;
+  const sent_by_fullname = 'Kirsten Lindsmith';
+  const sent_by_uuid = '656fa29d-454e-47ef-999e-4f624518b6f6';
+  //send a request to twilio, await a response
+  const twilio_id = '111fa29d-454e-47ef-999e-4f624518b6f6';
+  knex('messages').insert({
+    text,
+    sent_by_fullname,
+    sent_by_uuid,
+    twilio_id,
+    referral_uuid,
+  });
+});
 
 app.post('/receive-text', async (req, res) => {});
 
-app.get('/texts', (req, res) => {});
+app.get('/texts', (req, res) => {
+  const { referral_uuid } = req.body;
+});
 
 app.listen(port, () => {
   console.log(`Twilio demo backend listening on port ${port}`);
